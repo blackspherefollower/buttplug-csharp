c6931cd81f290e3574c898b6452aa9f046c637d3
blackspherefollower <darktech@black-sphere.co.uk>
2018-01-25 09:20:00 +0000

    Resharper/Code style fixes
    This should reduce the number of messages produced during static
    analysis.

--- Buttplug.Server/Bluetooth/BluetoothSubtypeManager.cs
+++ Buttplug.Server/Bluetooth/BluetoothSubtypeManager.cs
@@ -28,7 +28,6 @@ namespace Buttplug.Server.Bluetooth
                 new LovenseRev4BluetoothInfo(),
                 new LovenseRev5BluetoothInfo(),
                 new LovenseRev6BluetoothInfo(),
-                new LovenseRev7BluetoothInfo(),
                 new MagicMotionBluetoothInfo(),
                 new OhMiBodFuseBluetoothInfo(),
                 new VibratissimoBluetoothInfo(),
